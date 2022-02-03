d3f9a30b665fd8588662fd15443e34919212fd79
Eirik Albrigtsen <eirik.albrigtsen@truelayer.com>
2022-02-03 10:00:10 +0000

    build
    Signed-off-by: Eirik Albrigtsen <eirik.albrigtsen@truelayer.com>

--- export.sh
+++ export.sh
@@ -3,4 +3,4 @@
 # https://github.com/evilz/vscode-reveal/issues/799
 rm -rf docs/
 cp export docs -r
-sd  "http://localhost:45707" "/metrics" docs/index.html
+sd  "http://localhost:41431" "/metrics" docs/index.html
